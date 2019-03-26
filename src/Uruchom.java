dd1efc003888d5e85504af120d9c7a324302f477
Stasqq <stachocz@gmail.com>
2019-03-26 19:28:52 +0100

    First code

---
+++ src/Uruchom.java
@@ -0,0 +1,22 @@
+import Model.*;
+import View.*;
+import Controller.*;
+
+/**
+ * / Klasa służaca powołaniu do życia trzech obiektów
+ * / Odpowiednich do wzorca projektowego
+ * / Model, View, Controller
+ * / Zawiera funkcję main, uruchamia grę.
+ * /
+ */
+
+public class Uruchom {
+    /**
+     * funkcja main calej gry
+     */
+    public static void main(String args[]) {
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller();
+    }
+}
